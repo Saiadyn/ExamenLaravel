8fe982031ca63d129ee673969f02d94818b0097d
Saiadyn <78819183+Saiadyn@users.noreply.github.com>
2021-02-16 20:11:00 +0100

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,7 +5,7 @@
 @section('content')
     <h1 class="text-center">Bienvenido al inicio</h1>
     <div class="media border border-dark p-3 mt-2">
-        <img src="/imgs/examenIMG.jpeg" class="logos2 mr-3 mt-3 rounded-circle" style="width:200px">
+        <img src="/imgs/ExamenFlor2.jpeg" class="logos2 mr-3 mt-3 rounded-circle" style="width:200px">
         <div class="media-body">
           <p class="h4">Imagen de una flor</p>
           <p class="mt-2">
